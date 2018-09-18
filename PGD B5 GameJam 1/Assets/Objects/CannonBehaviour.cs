3ddbe7c92dec882fbaafd8d2a8d372bbf3638fb0
Ralph de Horde <ralphdehorde@ziggo.nl>
2018-09-18 12:32:42 +0200

    Shooting method and bullet

---
+++ PGD B5 GameJam 1/Assets/Objects/CannonBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonBehaviour : MonoBehaviour {
+
+	public GameObject hotDogPrefab;
+	public Transform hotDogSpawn;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonUp(0)) {
+			Shoot();
+		}
+	}
+
+	public void Shoot() {
+
+		var hotDog = (GameObject)Instantiate(hotDogPrefab, hotDogSpawn.position, hotDogSpawn.rotation);
+		var direction = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - hotDogSpawn.position).normalized;
+		
+		hotDog.GetComponent<Rigidbody2D>().velocity = direction;
+		Destroy(hotDog, 2.0f);
+	}
+	
+}
+
