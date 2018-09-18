21719cce67bf8e6cfa23f442ab3894155d8d3755
Metha <Methaan123@gmail.com>
2018-09-18 12:45:42 +0200

    Healthbar met player collision

---
+++ PGD B5 GameJam 1/Assets/Resources/HEALTHBAR.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HEALTHBAR : MonoBehaviour {
+
+
+    GameObject obj, obj2;
+    public GameObject player;
+    PlayerCollision playerInfo;
+
+    Sprite pixel;
+    float barSize = 50; 
+
+	// Use this for initialization
+	void Start () {
+        playerInfo = player.GetComponent<PlayerCollision>();
+
+        pixel = Resources.Load("Sprites/fillpixel", typeof(Sprite)) as Sprite;
+
+        obj = new GameObject("emptyBar");
+        SpriteRenderer render = obj.AddComponent(typeof(SpriteRenderer)) as SpriteRenderer;
+        render.sprite = pixel;
+        obj.transform.localScale = new Vector3(playerInfo.health, barSize, 1);
+
+        obj2 = new GameObject("fillBar");
+        SpriteRenderer render2 = obj2.AddComponent(typeof(SpriteRenderer)) as SpriteRenderer;
+        render2.sprite = pixel;
+        render2.color = new Color(255,0,0);
+
+        obj.transform.position = new Vector3(0, -2, -2);
+        obj2.transform.position = new Vector3(0, -2, -2);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(playerInfo.health >= playerInfo.damage)
+            obj2.transform.localScale = new Vector3(playerInfo.health - playerInfo.damage, barSize, 1);
+    }
+}
---
+++ PGD B5 GameJam 1/Assets/Resources/PlayerCollision.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+
+    public float damage = 0, health = 500;
+    public GameObject camera;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        camera.transform.position = transform.position - new Vector3(-1080/2/108, 0, 10);
+    }
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            damage += 1f;
+        }
+        if (collision.gameObject.tag == "EnemyCore")
+        {
+            damage += 2f;
+        }
+    }
+}
