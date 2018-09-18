2c772545c2454964263fa361b0ef026964248525
timo hondema <timohondema@hotmail.com>
2018-09-18 12:21:40 +0200

    Added enemies and spawning prefabs

---
+++ PGD B5 GameJam 1/Assets/Enemy Prefabs/Moving.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving : MonoBehaviour {
+
+
+	public float enemySpeed;
+	public int toTarget;
+	public Transform target;
+
+
+	// Use this for initialization
+	void Start()
+	{
+		enemySpeed = Random.value + 0.1f;
+		target = GameObject.Find("Player").transform;
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+
+		
+	}
+	void FixedUpdate()
+	{
+		Vector2 toTarget = target.transform.position - transform.position;
+		
+
+		transform.Translate(toTarget * enemySpeed * Time.deltaTime);
+
+		
+	}
+	
+
+}
